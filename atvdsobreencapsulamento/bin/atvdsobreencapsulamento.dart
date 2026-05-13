9f3286f6e420d4ffd8a4e888fa29e9a1f4e5ac91
GIOVANNI_DE_CASTRO_MORAIS_TRIVELLATO <22400087@DELTAF40.FLORESTA>
2026-05-13 07:38:35 -0300

    First commit

---
+++ atvdsobreencapsulamento/bin/atvdsobreencapsulamento.dart
@@ -0,0 +1,5 @@
+import 'package:atvdsobreencapsulamento/atvdsobreencapsulamento.dart' as atvdsobreencapsulamento;
+
+void main(List<String> arguments) {
+  print('Hello world: ${atvdsobreencapsulamento.calculate()}!');
+}
